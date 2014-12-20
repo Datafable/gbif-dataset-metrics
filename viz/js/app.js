916fe704e2a8dddceba6cbeec81e251a65c91a70
Peter Desmet <peter.desmet.work@gmail.com>
2014-12-20 10:53:47 +0100

    Working POC with downloads from the last year

--- viz/js/app.js
+++ viz/js/app.js
@@ -7,19 +7,61 @@ var main = function() {
 function getDownloads(datasetKey) {
     var url = "http://api.gbif.org/v1/occurrence/download/dataset/" + datasetKey + "?limit=100";
     console.log(url);
+    var url = "http://api.gbif.org/v1/occurrence/download/dataset/" + datasetKey + "?limit=1000";
     $.getJSON(url, function(data) {
-        var count = data["count"];
-        $("#downloadCount").html(count + " downloads"); // Add nice number formatting
+        var downloadCount = data["count"];
+        var occurrenceCount = 0;
 
-        var records = 0
+        var currentDate = new Date();
+        var startDate = new Date();
+            startDate.setUTCMonth(startDate.getUTCMonth() - 11); // Go back 11 months from current date (= a year ago)
+            startDate.setUTCDate("01"); // Set at first day of month
+            startDate.setUTCHours(0,0,0,0); // Set at midnight
+
+        var labels = [1,2,3,4,5,6,7,8,9,10,11,12]; // TODO: Should be dynamic.
+        var downloadsPerMonth = [0,0,0,0,0,0,0,0,0,0,0,0];
+        var occurrencesPerMonth = [0,0,0,0,0,0,0,0,0,0,0,0];
+
         $.each(data["results"],function(i,result) {
-            records = records + result["numberRecords"];
-            console.log(result["download"]["created"]);
+            occurrenceCount = occurrenceCount + result["numberRecords"];
+            var downloadDate = new Date(result["download"]["created"]);
+            var downloadMonth = downloadDate.getUTCMonth();
+
+            if (result["download"]["status"] == "SUCCEEDED" && downloadDate >= startDate) {
+                downloadsPerMonth[downloadMonth] = downloadsPerMonth[downloadMonth] + 1;
+                occurrencesPerMonth[downloadMonth] = occurrencesPerMonth[downloadMonth] + result["numberRecords"];
+            }
         });
-        $("#downloadRecordCount").html(records + " records"); // Add nice number formatting
 
+        $("#downloadCount").html(downloadCount + " downloads"); // TODO: Add nice number formatting
+        $("#downloadRecordCount").html(occurrenceCount + " records"); // TODO: Add nice number formatting
+
+
+        var data = {
+            "labels": labels,
+            "datasets": [
+                {
+                    "label": "Downloads",
+                    "fillColor": "rgba(220,220,220,0.5)",
+                    "strokeColor": "rgba(220,220,220,0.8)",
+                    "highlightFill": "rgba(220,220,220,0.75)",
+                    "highlightStroke": "rgba(220,220,220,1)",
+                    "data": downloadsPerMonth
+                },
+                // {
+                //     "label": "Occurrences",
+                //     "fillColor": "rgba(151,187,205,0.5)",
+                //     "strokeColor": "rgba(151,187,205,0.8)",
+                //     "highlightFill": "rgba(151,187,205,0.75)",
+                //     "highlightStroke": "rgba(151,187,205,1)",
+                //     "data": occurrencesPerMonth
+                // }
+            ]
+        };
+        var ctx = $("#downloadChart").get(0).getContext("2d");
+        var myBarChart = new Chart(ctx).Bar(data, {});
     });
-
 }
+
 
 $(document).ready(main);
