0d1328d59178ee18bc914b8edd99db2d3963252c
Peter Desmet <peter.desmet.work@gmail.com>
2014-12-30 11:51:16 +0100

    Time series and chart loading
    The animation is too jittery though

--- viz/js/app.js
+++ viz/js/app.js
@@ -1,10 +1,11 @@
 var main = function() {
     var datasetKey = "7f9eb622-c036-44c6-8be9-5793eaa1fa1e"; // Watervogels
-    var datasetKey = "271c444f-f8d8-4986-b748-e7367755c0c1"; // Florabank
+    // var datasetKey = "271c444f-f8d8-4986-b748-e7367755c0c1"; // Florabank
     // var datasetKey = "4fa7b334-ce0d-4e88-aaae-2e0c138d049e"; // eBird
 
     showTotalDownloads(datasetKey);
-    showDownloadsChart(datasetKey,30,1000);
+
+    createDownloadChart(datasetKey);
 };
 
 function showTotalDownloads(datasetKey) {
@@ -18,63 +19,86 @@ function showTotalDownloads(datasetKey) {
     });
 }
 
-function showDownloadsChart(datasetKey,dayRange,limit) {
+function createDownloadChart(datasetKey) {
+    var downloadChart = c3.generate({
+        bindto: "#downloadsChart",
+        data: {
+            x: "x",
+            columns: [
+                ["x","2014-12-13"],
+                ["downloads",12]
+            ],
+            type: "bar"
+        },
+        axis: {
+            x: {
+                type: "timeseries",
+                tick: {
+                    format: "%Y-%m-%d"
+                }
+            }
+        },
+        bar: {
+            width: {
+                ratio: 0.5
+            }
+        },
+        legend: {
+            show: false
+        }
+    });
+
+    getDownloads(datasetKey,new Date(),30,100,downloadChart); // Load data
+}
+
+function getDownloads(datasetKey,endDate,dayRange,limit,downloadChart) {
+    function midnightDateTime(date) {
+        return date.setUTCHours(0,0,0,0); // Floor date to midnight and return as time integer
+    }
+    function ISODateString(time) {
+        var date = new Date(time);
+        return date.toISOString().substring(0,10); // Return first 10 characters (yyyy-mm-dd) of full ISO date
+    }
+
+    var millisecondsRange = (dayRange - 1) * 24 * 3600 * 1000 // dayRange in milliseconds
+    var startDateTime = midnightDateTime(endDate) - millisecondsRange;
+    var endDateTime = midnightDateTime(endDate);
+    
     // Note: this function does only one call to the GBIF API, so if dayRange is high and limit low, it might not retrieve all downloads
     var url = "http://api.gbif.org/v1/occurrence/download/dataset/" + datasetKey + "?limit=" + limit;
-    console.log(url);
     d3.json(url,function(error,json) {
         if(error) return console.warn(error);
 
-        for (var i = 0, days = new Array(dayRange); i < dayRange;) days[i++] = null; // Create null filled array "days" with length dayRange
-        
-        var startDay = new Date();
-            startDay = getDayNumber(startDay) - dayRange + 1;
-
+        var days = {};
         json["results"].every(function(result) {
-            var downloadDate = new Date(result["download"]["created"]);
-            var downloadDay = getDayNumber(downloadDate);
-            if (downloadDay >= startDay) {
+            var downloadDateTime = midnightDateTime(new Date(result["download"]["created"]));
+            
+            if (downloadDateTime >= startDateTime) {
                 if (result["download"]["status"] == "SUCCEEDED") {
-                    days[downloadDay - startDay] += 1;
+                    var key = ISODateString(downloadDateTime); // Create key of form yyyy-mm-dd
+                    if (key in days) {
+                        days[key] += 1;
+                    } else {
+                        days[key] = 1; // Start new key
+                    }
                 }
                 return true; // Continue looping
             } else {
-                return false; // Stop looping, since loop passed beyond startDay. Assumes json is returned in reversed chronology.
+                return false; // Stop looping, since loop passed beyond startDateTime. Assumes API returns downloads in reversed chronology.
             }
         });
 
-        // Create chart
-        var chart = c3.generate({
-            bindto: "#downloadsChart",
-            data: {
-                columns: [
-                    ["downloads"].concat(days)
-                ],
-                type: "bar"
-            },
-            axis: {
-                x: {
-                    show: false
-                },
-                y: {
-                    tick: {
-                        format: function(x) {
-                            return (x == Math.floor(x)) ? x : "";
-                        }
-                    }
-                }
-            },
-            legend: {
-                show: false
-            },
-            interaction: {
-                enabled: false
-            },
-            bar: {
-                width: {
-                    ratio: 1
-                }
-            }
+        var daysKeys = Object.keys(days);
+        var daysValues = new Array;
+        for (var i = 0; i < daysKeys.length; i++) {
+            daysValues[i] = days[daysKeys[i]];
+        }
+
+        downloadChart.load({
+            columns: [
+                ["x"].concat(daysKeys),
+                ["downloads"].concat(daysValues)
+            ]
         });
     });
 }
