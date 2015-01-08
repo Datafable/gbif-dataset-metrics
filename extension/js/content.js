0ec4ed2dc5f6ff124f96b930521fb34cd1b1d52b
Peter Desmet <peter.desmet.work@gmail.com>
2015-01-08 14:39:34 +0100

    Use function expressions instead of declarations

--- extension/js/content.js
+++ extension/js/content.js
@@ -1,7 +1,24 @@
-function removeDownloadBox() {
+var main = function() {
+    console.log(getDatasetKeyFromURL());
+    removeDownloadBox();
+    addChart();
+    console.log(document.URL);
+}
+
+var getDatasetKeyFromURL = function() {
+    var pathArray = window.location.pathname.split('/'); // /dataset/key/*
+    var datasetKey = pathArray[2]; // 0 is empty, second is "dataset"
+    return datasetKey;
+}
+
+var removeDownloadBox = function() {
     var downloadBox = $("#infoband").find(".content .box");
     downloadBox.fadeOut(2000);
 }
 
-removeDownloadBox();
-console.log(document.URL);
+var addChart = function() {
+    var hook = $("#content").find(".content .header");
+    hook.after('<div id="downloadChart">Hello</div>').addClass("red");
+}
+
+$(document).ready(main);
