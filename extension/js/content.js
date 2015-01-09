38fe3314e222b740bffbf3778b85ad09eacbe941
Peter Desmet <peter.desmet.work@gmail.com>
2015-01-09 23:18:47 +0100

    Add download charts to GBIF activity page

--- extension/js/content.js
+++ extension/js/content.js
@@ -1,24 +1,5 @@
-var main = function() {
-    console.log(getDatasetKeyFromURL());
-    removeDownloadBox();
-    addChart();
-    console.log(document.URL);
-}
-
 var getDatasetKeyFromURL = function() {
-    var pathArray = window.location.pathname.split('/'); // /dataset/key/*
+    var pathArray = window.location.pathname.split('/');
     var datasetKey = pathArray[2]; // 0 is empty, second is "dataset"
     return datasetKey;
 }
-
-var removeDownloadBox = function() {
-    var downloadBox = $("#infoband").find(".content .box");
-    downloadBox.fadeOut(2000);
-}
-
-var addChart = function() {
-    var hook = $("#content").find(".content .header");
-    hook.after('<div id="downloadChart">Hello</div>').addClass("red");
-}
-
-$(document).ready(main);
