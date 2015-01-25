645b31f41c58a04483fb88c43368674569142ae0
Bart Aelterman <bart.aelterman@gmail.com>
2015-01-25 20:41:53 +0100

    Add comments

--- src/aggregator.py
+++ src/aggregator.py
@@ -2,11 +2,23 @@ from glob import glob
 import json
 
 class ReportAggregator():
+    """
+    This class will be used to aggregate gbif_extract*.report.json files.
+    Metric data will be aggregated by data set key
+    """
+
     def find_files(self, folder):
+        """
+        Look for all gbif_extract*.report.json files
+        in a given folder.
+        """
         files = glob(folder + '/gbif_extract*.report.json')
         return files
 
     def merge_data_set_in_metrics(self, dataset, metrics_data):
+        """
+        merge the metric data by metric type and metric name
+        """
         for metric_type in dataset.keys():
             if metric_type in metrics_data.keys():
                 for metric_name in dataset[metric_type].keys():
@@ -20,6 +32,11 @@ class ReportAggregator():
                 
 
     def aggregate(self, data_folder):
+        """
+        iterate over all the gbif_extract*.report.json files
+        and merge the metrics data by dataset key and
+        metric type
+        """
         files = self.find_files(data_folder)
         metrics = {}
         for f in files:
