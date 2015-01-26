f1307233e57bf34a48ea5edd626ae94632e4c37c
Bart Aelterman <bart.aelterman@gmail.com>
2015-01-26 16:06:39 +0100

    let test script create fixture data

--- src/test_aggregate_reports.py
+++ src/test_aggregate_reports.py
@@ -1,20 +1,75 @@
 import unittest
 import os
 import sys
+import json
 PROJECT_PATH = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 sys.path.append(PROJECT_PATH + '/src')
 import aggregator
 
 class TestAggregator(unittest.TestCase):
+    def createFixtureData(self):
+        data = [
+        {
+            '3F2504E0-4F89-11D3-9A0C-0305E82C3301': {
+                'BASISOFRECORDS': { 'HUMAN_OBSERVATION': 3710, 'FOSSIL_SPECIMEN': 3 },
+                'NUMBER_OF_RECORDS': 3713
+            },
+            '05ebc824-3a3b-4f64-ab22-99b0e2c3aa48': {
+                'BASISOFRECORDS': { 'UNKNOWN': 6695 },
+                'NUMBER_OF_RECORDS': 6695
+            },
+            '82746a3e-f762-11e1-a439-00145eb45e9a': {
+                'BASISOFRECORDS': { 'FOSSIL_SPECIMEN': 532, 'UNKNOWN': 10 },
+                'NUMBER_OF_RECORDS': 542
+            }
+        },
+        {
+            '3F2504E0-4F89-11D3-9A0C-0305E82C3301': {
+                'BASISOFRECORDS': { 'HUMAN_OBSERVATION': 10134, 'UNKNOWN': 7 },
+                'NUMBER_OF_RECORDS': 10141
+            },
+            '05ebc824-3a3b-4f64-ab22-99b0e2c3aa48': {
+                'BASISOFRECORDS': { 'UNKNOWN': 89963 },
+                'NUMBER_OF_RECORDS': 89963
+            },
+            '82746a3e-f762-11e1-a439-00145eb45e9a': {
+                'BASISOFRECORDS': { 'UNKNOWN': 130 },
+                'NUMBER_OF_RECORDS': 130
+            }
+        },
+        {
+            '3F2504E0-4F89-11D3-9A0C-0305E82C3301': {
+                'BASISOFRECORDS': { 'MACHINE_OBSERVATION': 5, 'UNKNOWN': 9 },
+                'NUMBER_OF_RECORDS': 14
+            },
+            '82746a3e-f762-11e1-a439-00145eb45e9a': {
+                'BASISOFRECORDS': { 'UNKNOWN': 137 },
+                'NUMBER_OF_RECORDS': 137
+            }
+        }
+        ]
+        self.test_dir = os.path.join(PROJECT_PATH, 'data')
+        if not os.path.exists(self.test_dir):
+            os.makedirs(self.test_dir)
+        for index, data_blob in enumerate(data):
+            file_name = os.path.join(self.test_dir, 'gbif_extract{0}.json'.format(index))
+            self.test_files.append(file_name)
+            f = open(file_name, 'wb')
+            f.write(json.dumps(data_blob))
+            f.close()
+
     def setUp(self):
+        self.test_files = []
         self.agg = aggregator.ReportAggregator()
-        self.data_folder = PROJECT_PATH + '/data'
+        self.createFixtureData()
+
+    def tearDown(self):
+        for f in self.test_files:
+            os.remove(f)
 
     def test_find_files(self):
-        expected_file_names = ['gbif_extract1.report.json', 'gbif_extract2.report.json', 'gbif_extract3.report.json']
-        expected_files = [os.path.join(self.data_folder, x) for x in expected_file_names]
-        files = self.agg.find_files(self.data_folder)
-        self.assertEqual(files, expected_files)
+        files = self.agg.find_files(self.test_dir)
+        self.assertEqual(files, self.test_files)
 
     def test_merge_data(self):
         outdata = {'metric_type1': {'metric1': 10, 'metric2': 2}}
@@ -34,7 +89,7 @@ class TestAggregator(unittest.TestCase):
         fossil_specimen = 3
         machine_observation = 5
         nr_of_records = 3710 + 10134 + 7 + 9 + 3 + 5
-        aggregated_metrics = self.agg.aggregate(self.data_folder)
+        aggregated_metrics = self.agg.aggregate(self.test_dir)
         self.assertEqual(aggregated_metrics[dataset_key]['BASISOFRECORDS']['HUMAN_OBSERVATION'], human_observations)
         self.assertEqual(aggregated_metrics[dataset_key]['BASISOFRECORDS']['UNKNOWN'], unknown)
         self.assertEqual(aggregated_metrics[dataset_key]['BASISOFRECORDS']['FOSSIL_SPECIMEN'], fossil_specimen)
