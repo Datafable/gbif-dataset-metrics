dc83ef3e4ed7720e88646491e3c2696a425b6bdd
Bart Aelterman <bart.aelterman@gmail.com>
2015-01-31 22:49:24 +0100

    Add scripts to merge taxonomy

--- bin/fetch_taxonomy.sh
+++ bin/fetch_taxonomy.sh
@@ -8,5 +8,5 @@
 #    $100: genus
 #    $220: species
 grep -v "gbifID" $1 | awk 'BEGIN {FS="\t"} {OFS="\t"} {print $197,$128,$165,$67,$158,$93,$100,$220}' | sort | uniq -c > temp_taxonomy.txt
-python src/merge_taxonomy_2icicle.py temp_taxonomy.txt settings.json
+python bin/merge_taxonomy.py temp_taxonomy.txt settings.json
 #rm temp_taxonomy.txt
