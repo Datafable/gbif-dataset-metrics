3ee4a4773a9aa60d496ddb819e8e37e2534ad947
Peter Desmet <peter.desmet.work@gmail.com>
2015-02-02 23:12:53 +0100

    Add taxonomy partition to demo
    * Based on file (change this)
    * Using color brewer (change to make own colours)

--- extension/js/stats.js
+++ extension/js/stats.js
@@ -18,8 +18,61 @@ var addMetricsToStatsPage = function (metrics) {
     // Activate bar metrics tooltip
     $('[data-toggle="tooltip"]').tooltip();
 
+    // Add taxonomy partition
+    html = '<article id="taxonomy">' + 
+                '<header></header>' + 
+                '<div class="content">' + 
+                    '<div class="header"><div class="left"><h2>Taxonomy</h2></div></div>' +
+                    '<div class="fullwidth"></div>' +
+                '</div>' +
+                '<footer></footer>' + 
+            '</article>';
+    anchor.after(html);
+    taxonomyPartition(metrics);
+};
 
+var taxonomyPartition = function (metrics) {
+    // Based on http://bl.ocks.org/mbostock/1005873
+    var width = 874,
+        height = 250;
 
+    var x = d3.scale.linear().range([0, width]),
+        y = d3.scale.linear().range([0, height]),
+        color = d3.scale.ordinal().range(colorbrewer.Blues[5]); // BuGn, YlGn, YlGnBu, PuBu, Blues, RdYlGn
+
+    var partition = d3.layout.partition()
+        .children(function (d) { return isNaN(d.value) ? d3.entries(d.value) : null; })
+        .value(function (d) { return d.value; });
+
+    var svg = d3.select("#taxonomy .fullwidth").append("svg")
+        .attr("width", width)
+        .attr("height", height);
+
+    var rect = svg.selectAll("rect");
+
+    d3.json("taxonomy.json", function (error, root) {
+        rect = rect
+            .data(partition(d3.entries(root)[0]))
+            .enter().append("rect")
+            .attr("x", function (d) { return x(d.x); })
+            .attr("y", function (d) { return y(d.y); })
+            .attr("width", function (d) { return x(d.dx); })
+            .attr("height", function (d) { return y(d.dy); })
+            .attr("fill", function (d) { return color((d.children ? d : d.parent).key); })
+            .on("click", clicked);
+    });
+
+    function clicked (d) {
+        x.domain([d.x, d.x + d.dx]);
+        // y.domain([d.y, 1]).range([d.y ? 20 : 0, height]);
+
+        rect.transition()
+            .duration(600)
+            .attr("x", function (d) { return x(d.x); })
+            //.attr("y", function (d) { return y(d.y); })
+            .attr("width", function (d) { return x(d.x + d.dx) - x(d.x); })
+            //.attr("height", function (d) { return y(d.y + d.dy) - y(d.y); });
+    }
 };
 
 main();
