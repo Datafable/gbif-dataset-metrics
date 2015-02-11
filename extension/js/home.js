2f5461f9096cd128cf86bf29000fb2945b74b8bd
Peter Desmet <peter.desmet.work@gmail.com>
2015-02-11 17:37:01 +0100

    Try salvattore
    + Lightweight library
    - Images get distributed evenly over columns, instead of taking into
    account their height => Some columns much higher than others

--- extension/js/home.js
+++ extension/js/home.js
@@ -8,9 +8,6 @@ var addMetricsToHomePage = function (metrics) {
     var html = "";
     // Basis of record
     html = html + basisOfRecordBar(metrics);
-
-    html = html + imageThumbnails();
-
     // Add HTML to page
     $("#summary").find(".content").prepend(html);
 
@@ -24,7 +21,12 @@ var imageThumbnails = function (metrics) {
         1052605230: "https://farm8.staticflickr.com/7545/16198002052_dcc548d4eb.jpg",
         1052605234: "https://farm9.staticflickr.com/8612/15578712533_7356631694.jpg",
         1052605311: "http://lh3.ggpht.com/-2wXMbN54vRE/VKodWTAzTqI/AAAAAAAAEDM/IwtsE-K5LxU/s512/IMG_2608.JPG",
-        1052605303: "http://static.inaturalist.org/photos/1464583/medium.JPG?1420433811"
+        1052605303: "http://static.inaturalist.org/photos/1464583/medium.JPG?1420433811",
+        1052605359: "http://static.inaturalist.org/photos/1464709/medium.JPG?1420437838",
+        1052605333: "http://static.inaturalist.org/photos/1464708/medium.JPG?1420437814",
+        1052605384: "http://static.inaturalist.org/photos/1464784/medium.JPG?1420440139",
+        1052605366: "http://static.inaturalist.org/photos/1464727/medium.JPG?1420438564",
+        1052605339: "http://static.inaturalist.org/photos/1464691/medium.JPG?1420436811"
     };
     var thumbnails = [];
 
