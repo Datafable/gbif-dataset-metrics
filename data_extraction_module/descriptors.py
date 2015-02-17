2c2798ffe501242d18b2c01d080034783c692443
Nicolas Noé <nicolas@niconoe.eu>
2015-02-17 23:15:54 +0100

    Implemented #63.

--- data_extraction_module/descriptors.py
+++ data_extraction_module/descriptors.py
@@ -7,7 +7,17 @@ class DatasetDescriptor(object):
                      'BASISOFRECORDS': {},
                      'TAXON_MATCHES': {},
                      'TAXONOMY': {},
-                     'COORDINATE_QUALITY_CATEGORIES': {}}
+                     'COORDINATE_QUALITY_CATEGORIES': {},
+
+                     'MEDIA': {'media_url_invalid': 0,
+                               'media_not_provided': 0,
+                               'media_valid': 0,
+                               'movingimage': {},
+                               'audio': {},
+                               'stillimage': {},
+                               'no_type': {},
+                               }
+                     }
 
     def increment_number_records(self):
         self.data['NUMBER_OF_RECORDS'] = self.data['NUMBER_OF_RECORDS'] + 1
@@ -29,6 +39,33 @@ class DatasetDescriptor(object):
 
     def store_or_increment_coordinatecategory(self, value):
         self._store_or_increment_counter(value, 'COORDINATE_QUALITY_CATEGORIES')
+
+    def mul_increment_invalid_url_count(self):
+        self.data['MEDIA']['media_url_invalid'] = self.data['MEDIA']['media_url_invalid'] + 1
+
+    def mul_increment_not_provided_count(self):
+        self.data['MEDIA']['media_not_provided'] = self.data['MEDIA']['media_not_provided'] + 1
+
+    def mul_increment_valid_count(self):
+        self.data['MEDIA']['media_valid'] = self.data['MEDIA']['media_valid'] + 1
+
+    def _mul_add_occurrence(self, container, occurrence_id, reference):
+        if occurrence_id not in container:
+            container[occurrence_id] = [reference]
+        else:
+            container[occurrence_id].append(reference)
+
+    def mul_add_image(self, occurrence_id, reference):
+        self._mul_add_occurrence(self.data['MEDIA']['stillimage'], occurrence_id, reference)
+
+    def mul_add_video(self, occurrence_id, reference):
+        self._mul_add_occurrence(self.data['MEDIA']['movingimage'], occurrence_id, reference)
+
+    def mul_add_audio(self, occurrence_id, reference):
+        self._mul_add_occurrence(self.data['MEDIA']['audio'], occurrence_id, reference)
+
+    def mul_add_notype(self, occurrence_id, reference):
+        self._mul_add_occurrence(self.data['MEDIA']['no_type'], occurrence_id, reference)
 
 
 class DatasetDescriptorAwareEncoder(json.JSONEncoder):
